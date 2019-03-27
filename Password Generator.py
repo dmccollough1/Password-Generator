31009bc489a6aa35bf70b44c10ef40072e06f075
Don_M <36451343+dmccollough1@users.noreply.github.com>
2019-03-27 17:05:58 -0500

    Update Password Generator.py
    Updated the variables so that instead of generating a random (pattern and length) string between 8 and 16 characters, it now generates a random (pattern and length) between 12 and 20 characters to increase the security of the password being generated.

--- Password Generator.py
+++ Password Generator.py
@@ -1,8 +1,8 @@
-import string
+import random, string
 from random import *
 print ("\n")
 print ("DISCLAIMER! This program Will Not store or save these passwords! \nPlease Keep these in a secure place.\n")
 print ("Generating Random Password \n")
 characters = string.ascii_letters + string.punctuation  + string.digits
-password =  "".join(choice(characters) for x in range(randint(8, 16)))
+password =  "".join(choice(characters) for x in range(randint(12, 20)))
 print (password)
