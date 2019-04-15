1bd0edc3228806a230b0578406a9ddba96492833
Don_McCollough <36451343+dmccollough1@users.noreply.github.com>
2019-04-15 17:41:01 -0500

    Update Password Generator.py
    Updating to version 0.1.2
     - Increased password lengths from 8 and 16 to 12 and 20 for improved security

--- Password Generator.py
+++ Password Generator.py
@@ -1,8 +1,13 @@
+# Importing random for the int value and string for the output of the password
 import random, string
 from random import *
-print ("\n")
+
+# Print disclaimer for user information
 print ("DISCLAIMER! This program Will Not store or save these passwords! \nPlease Keep these in a secure place.\n")
 print ("Generating Random Password \n")
 characters = string.ascii_letters + string.punctuation  + string.digits
+
+# Combine characters and display the password
+# Version 0.1.2 - Updated to support longer passwords for strength and security. Password length is now between 12 and 20 characters of alphanumberic and special characters.
 password =  "".join(choice(characters) for x in range(randint(12, 20)))
 print (password)
